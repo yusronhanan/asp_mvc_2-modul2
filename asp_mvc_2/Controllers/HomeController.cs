813af7290ccc00d08bcdf3440eca024531aff55d
yusronhanan <yusronzain@gmail.com>
2017-02-09 09:31:01 +0700

    Add project files.

---
+++ asp_mvc_2/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using asp_mvc_2.Security;
+
+namespace asp_mvc_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        [AuthorizeRole("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ asp_mvc_2/Security/AuthorizeRoleAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using asp_mvc_2.Models.DB;
+using asp_mvc_2.Models.EntityManager;
+
+
+namespace asp_mvc_2.Security
+{
+    public class AuthorizeRoleAttribute : AuthorizeAttribute
+    {
+        private readonly string[] userAssignedRoles;
+        public AuthorizeRoleAttribute(params string[] roles)
+        {
+            this.userAssignedRoles = roles;
+        }
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorize = false;
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                UserManager UM = new UserManager();
+                foreach (var roles in userAssignedRoles)
+                {
+                    authorize = UM.IsUserInRole(httpContext.User.Identity.Name, roles);
+                    if (authorize)
+                        return authorize;
+                }
+            }
+            return authorize;
+        }
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Home/UnAuthorized");
+        }
+    }
+}
